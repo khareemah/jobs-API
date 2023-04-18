d38002c51de38f15dcb05b33b8653d8c3094122d
kareemah <ajimobikareemah@gmail.com>
2023-04-18 13:04:35 +0700

    swagger documentation added

--- app.js
+++ app.js
@@ -5,36 +5,44 @@ const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
 
+// extra security packages
 const cors = require('cors');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+// app routes
 const authRouter = require('./routes/authRoute');
 const authenticateUser = require('./middleware/authenticateUser');
 const jobsRouter = require('./routes/jobsRoute');
 const notFoundMiddleware = require('./middleware/notfound');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
-
-app.get('/', (req, res) => res.send('Home...'));
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
-
-// security packages
 app.set('trust proxy', 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000,
-    max: 10,
+    max: 100,
   })
 );
+app.use(express.json());
+
+app.get('/', (req, res) =>
+  res.send('<h1>Jobs API</h1><a href="api-docs">Documentation</a>')
+);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.use(notFoundMiddleware);
 app.use(cors);
 app.use(helmet);
 app.use(xss);
+app.use(errorHandlerMiddleware);
+
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
