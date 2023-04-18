dd7c04bcc11003100510919219de62ea401141b6
kareemah <ajimobikareemah@gmail.com>
2023-04-18 18:36:07 +0700

    add key

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
 // swagger
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+// const swaggerUI = require('swagger-ui-express');
+// const YAML = require('yamljs');
+// const swaggerDocument = YAML.load('./swagger.yaml');
 // app routes
 const authRouter = require('./routes/authRoute');
 const authenticateUser = require('./middleware/authenticateUser');
@@ -34,7 +34,7 @@ app.use(express.json());
 app.get('/', (req, res) =>
   res.send('<h1>Jobs API</h1><a href="api-docs">Documentation</a>')
 );
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 app.use(notFoundMiddleware);
